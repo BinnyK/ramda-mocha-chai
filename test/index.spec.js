421746ee68a69fcec9b00b4702ed093c09403775
BinnyK <BinnyK@users.noreply.github.com>
2017-06-14 09:05:48 +1000

    Initial commit with all dependencies in package json

---
+++ test/index.spec.js
@@ -0,0 +1,12 @@
+// var assert = require('chai').assert;
+var expect = require('chai').expect;
+// var should = require('chai').should;
+
+import R from 'ramda';
+
+describe('Initial test', () => {
+  it('should work...', () => {
+    expect('working!').to.equal('working!')
+  });  
+})
+
